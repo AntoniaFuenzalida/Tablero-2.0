34a49f676b8722e6237dccf373e3f97010055de9
AntoniaFuenzalida <afuenzalida20@alumnos.utalca.cl>
2025-05-19 14:26:14 -0400

    Actualización base de datos

--- db.sql
+++ db.sql
@@ -1,40 +1,57 @@
 USE tablero;
 
-CREATE TABLE Usuario (
+-- Tabla Usuario
+CREATE TABLE IF NOT EXISTS Usuario (
   id INT AUTO_INCREMENT PRIMARY KEY,
   nombre VARCHAR(100),
   correo VARCHAR(100),
   contraseña VARCHAR(100),
   departamento VARCHAR(100),
   oficina VARCHAR(100),
-  rol VARCHAR(20)
+  rol VARCHAR(20),
   disponible BOOLEAN DEFAULT FALSE
 );
 
-CREATE TABLE Tablero (
+-- Tabla Tablero
+CREATE TABLE IF NOT EXISTS Tablero (
   id INT AUTO_INCREMENT PRIMARY KEY,
   usuario_id INT,
   FOREIGN KEY (usuario_id) REFERENCES Usuario(id)
 );
 
-CREATE TABLE TableroCadenasTexto (
+-- Tabla TableroCadenasTexto
+CREATE TABLE IF NOT EXISTS TableroCadenasTexto (
   id INT AUTO_INCREMENT PRIMARY KEY,
   tablero_id INT,
   texto VARCHAR(255),
   FOREIGN KEY (tablero_id) REFERENCES Tablero(id)
 );
 
-CREATE TABLE TableroInfoConexion (
+-- Tabla TableroInfoConexion
+CREATE TABLE IF NOT EXISTS TableroInfoConexion (
   id INT AUTO_INCREMENT PRIMARY KEY,
   tablero_id INT,
   info TEXT,
   FOREIGN KEY (tablero_id) REFERENCES Tablero(id)
 );
 
-CREATE TABLE DiaAtencion (
+-- Tabla DiaAtencion
+CREATE TABLE IF NOT EXISTS DiaAtencion (
   id INT AUTO_INCREMENT PRIMARY KEY,
   diaSemana VARCHAR(20),
   hora TIME,
   usuario_id INT,
   FOREIGN KEY (usuario_id) REFERENCES Usuario(id)
+);
+
+-- Tabla Notificacion
+CREATE TABLE IF NOT EXISTS Notificacion (
+  id INT NOT NULL AUTO_INCREMENT,
+  usuarioId INT NOT NULL,
+  mensaje TEXT NOT NULL,
+  tipo VARCHAR(50) DEFAULT NULL,
+  leida TINYINT(1) DEFAULT '0',
+  fecha TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id),
+  FOREIGN KEY (usuarioId) REFERENCES Usuario(id)
 );
